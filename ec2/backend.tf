247ac7ce1aed2829daaaa891605b280f021f5180
sonali-varshney <varshneysonali28@gmail.com>
2025-09-07 13:39:26 +0530

    ec2

--- ec2/backend.tf
+++ ec2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "test-s3-terraform-use-case1" 
-    key    = "dev/terraform.tfstate"
+    key    = "dev/terraform.tfstate"    #Note  we have to give terraform.tfstate
     region = "us-east-1"
   }
 }
