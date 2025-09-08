9651aeb496f93b6f4f4e03afef8f2490384dadae
sonali-varshney <44549506+sonali-varshney@users.noreply.github.com>
2025-09-08 14:41:00 +0530

    Update backend.tf

--- useCase2RDS/vpc/backend.tf
+++ useCase2RDS/vpc/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "test-s3-terraform-use-case1" 
     key    = "dev/terraform.tfstate"    #Note  we have to give terraform.tfstate
     region = "us-east-1"
-    use_lockfile = true
+    #use_lockfile = true
   }
 }
